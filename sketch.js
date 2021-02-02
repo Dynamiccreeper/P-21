a83142150f9339b3cf9ff0c3f7844903c0e91e4a
Dynamiccreeper <74767888+Dynamiccreeper@users.noreply.github.com>
2021-02-02 15:11:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,58 @@
+var bullet;
+var wall,thickness;
+var speed;
+var weight;
+
+function setup() {
+  createCanvas(1600,400);
+
+  speed=random(55,90);
+  weight=random(500,1500);
+  thickness = random(22,83);
+
+bullet = createSprite(50, 200, 50, 10);
+
+bullet.velocityX=speed;
+
+bullet.shapeColor = color(255);
+
+
+wall=createSprite(1200,200,thickness,height/2);
+wall.shapeColor=color(80,80,80);
+
+
+}
+
+function draw() {
+  background(0);  
+if(isBouncing(wall,bullet))
+{
+
+  bullet.velocityX=0;
+  var damage = 0.5 * weight * speed* speed/(thickness * thickness*thickness);
+  if(damage>10){
+
+    wall.shapeColor = color(255,0,0);
+  }
+
+
+
+  if(damage<10){
+
+    wall.shapeColor = color(0,255,0);
+  }
+}
+  drawSprites();
+}
+
+function isBouncing (wall,bullet){
+bulletRightEdge=bullet=bullet.x + bullet.width;
+wallLeftEdge=wall.x;
+if(bulletRightEdge>=wallLeftEdge)
+{
+  return true;
+}
+return false;
+
+
+}
